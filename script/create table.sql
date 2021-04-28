6fb505014917a960c78171903ee497bb58d3a1af
wikopratama <wikoo.adha@gmail.com>
2021-04-28 10:28:50 +0800

    Add DB Script

---
+++ script/create table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `patient` (
+  `id` bigint NOT NULL,
+  `email_address` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
